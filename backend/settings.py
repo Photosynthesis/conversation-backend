755afb9269dcddb13d0d1f1994dbcafebe80fea4
AdamM <adam@photosynthesis.ca>
2023-07-29 20:16:46 +0200

    Add static root

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-#=g#rqj#esce2-f&$ebk56usbzx7whr*mj5%uj_t^lyvsb^dw$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['adammck.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost','adammck.pythonanywhere.com']
 
 # Application definition
 
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
